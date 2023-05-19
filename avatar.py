48410d41dfa78fad03af4c6f828b9883eeff80b3
Paula Asto <paulaasto@bcp.com.pe>
2023-05-19 16:02:37 -0500

    Adding customize avatar to new branch

--- avatar.py
+++ avatar.py
@@ -1,3 +1,10 @@
-from py_avataaars import PyAvataaar
-avatar = PyAvataaar()
+import py_avataaars as pa
+avatar = pa.PyAvataaar(
+    style=pa.AvatarStyle.CIRCLE,
+    skin_color=pa.SkinColor.LIGHT,
+    hair_color=pa.HairColor.AUBURN,
+    top_type=pa.TopType.SHORT_HAIR_SHAGGY_MULLET,
+    nose_type=pa.NoseType.DEFAULT,
+    accessories_type=pa.AccessoriesType.PRESCRIPTION_02,
+)
 avatar.render_png_file('avatar1.png')
