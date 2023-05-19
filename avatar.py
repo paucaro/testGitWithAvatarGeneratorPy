4c97a03debd865ec49ec2c369f0ed3298519d7a1
Paula Asto <paulaasto@bcp.com.pe>
2023-05-19 16:26:47 -0500

    Añadiendo nuevas funcionalidades a mi avatar

--- avatar.py
+++ avatar.py
@@ -6,5 +6,11 @@ avatar = pa.PyAvataaar(
     top_type=pa.TopType.SHORT_HAIR_SHAGGY_MULLET,
     nose_type=pa.NoseType.DEFAULT,
     accessories_type=pa.AccessoriesType.PRESCRIPTION_02,
+    facial_hair_type=pa.FacialHairType.MOUSTACHE_MAGNUM,
+    mouth_type=pa.MouthType.SCREAM_OPEN,
+    eye_type=pa.EyesType.SQUINT,
+    eyebrow_type=pa.EyebrowType.RAISED_EXCITED_NATURAL,
+    clothe_type=pa.ClotheType.HOODIE,
+    clothe_graphic_type=pa.ClotheGraphicType.BAT,
 )
 avatar.render_png_file('avatar1.png')
